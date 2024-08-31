b578627ab292310a9f2fbeb0939e2cd889618519
feras <fsalmasri@hotmail.com>
2024-08-31 20:41:24 +0200

    Complete conversion to LS

--- mainpulation_tools/connect_LS.py
+++ mainpulation_tools/connect_LS.py
@@ -3,12 +3,19 @@ import os
 import numpy as np
 from label_studio_sdk.client import LabelStudio
 from PIL import Image
+import uuid
+import secrets
+import string
+from tqdm import tqdm
 
 
 LABEL_STUDIO_URL = 'http://localhost:8080'
 API_KEY = '19563f95209292aa545aa0845388c18ad476f30c'
 ls = LabelStudio(base_url=LABEL_STUDIO_URL, api_key=API_KEY)
 
+imgs_dir = r'C:\Users\fsalm\Desktop\projects\ULB\UGCT\UG_CT_images\imgs'
+labels_dir = r'C:\Users\fsalm\Desktop\projects\ULB\UGCT\UG_CT_images\labels'
+classes_file = r'C:\Users\fsalm\Desktop\projects\ULB\UGCT\UG_CT_images\classes.txt'
 
 def create_project():
     pass
@@ -21,16 +28,22 @@ def get_project_by_name(project_name):
     print(f'Number of projects: {number_of_projects}')
     for proj in project_list:
         if proj.title == project_name:
-            return ls.projects.get(id=proj.id), id
+            return ls.projects.get(id=proj.id), proj.id
 
 
 def get_tasks(proj_id):
-    return ls.tasks.list(project=1)
+    tasks = ls.tasks.list(project=proj_id)
+    tasks_list = list(tasks)
+    number_of_tasks = len(tasks_list)
 
-def create_tasks(proj_id, pkg_name, img_name):
+    print(f'Number of tasks: {number_of_tasks}')
+
+    return tasks
+
+def create_task(proj_id, folder_name, img_name):
     ls.tasks.create(
         project=proj_id,
-        data={"image": f"data/local-files?d={pkg_name}/{img_name}", "text": f"{img_name}"}
+        data={"image": f"data/local-files?d={folder_name}/{img_name}", "text": f"{img_name}"}
     )
 
 def update_task(proj_id, task_id, pkg_name, img_name):
@@ -43,71 +56,105 @@ def update_task(proj_id, task_id, pkg_name, img_name):
 project, proj_id = get_project_by_name('Package 1')
 
 
-# imgs_dir = r'C:\Users\fsalm\Desktop\projects\ULB\UGCT\UG_CT_images\imgs'
-# labels_dir = r'C:\Users\fsalm\Desktop\projects\ULB\UGCT\UG_CT_images\labels'
-# classes_file = r'C:\Users\fsalm\Desktop\projects\ULB\UGCT\UG_CT_images\classes.txt'
-#
-# im_name = '1.1_RCNX0012.JPG'
-# root, _ = os.path.splitext(im_name)
-# lbl_name = f'{root}.txt'
-#
-#
-# print(im_name, lbl_name)
-# img = np.array(Image.open(os.path.join(imgs_dir, im_name)))
-# h,w,_ = img.shape
-# data = []
-# with open(os.path.join(labels_dir, lbl_name), 'r') as file:
-#     for line in file:
-#         # Split the line by spaces and convert each part to a float
-#         numbers = list(map(float, line.split()))
-#         # Append the list of numbers to the data list
-#         data.append(numbers)
-#
-#
-# classes = []
-# with open(classes_file, 'r') as file:
-#     for line in file:
-#         classes.append(line.strip())
-#
-#
-# results = []
-# for item in data:
-#     class_name = classes[int(item[0])]
-#
-#     width = item[3]
-#     height = item[4]
-#     x = item[1] - (width / 2)
-#     y = item[2] - (height / 2)
-#
-#     x *= 100
-#     y *= 100
-#     width *= 100
-#     height *= 100
-#
-#     results.append({
-#                 "id": "unique_id_123",
-#                 "type": "rectanglelabels",
-#
-#                 "value": {
-#                 "x": x,
-#                 "y": y,
-#                 "width": width,
-#                 "height": height,
-#                 "rotation": 0,
-#                 "rectanglelabels": [class_name]
-#                 },
-#                 "origin": "manual",
-#                 "to_name": "image",
-#                 "from_name": "label",
-#
-#                 "image_rotation": 0,
-#                 "original_width": w,
-#                 "original_height": h
-#             })
-#
-# ls.annotations.create(
-#     id=1,
-#     result= results,
-#     was_cancelled=False,
-#     ground_truth=True,
-# )
+def add_tasks_from_folder(proj_id, imgs_dir, folder_name):
+    flst = os.listdir(imgs_dir)
+
+    for f in flst:
+        create_task(proj_id, folder_name, img_name=f)
+
+
+
+def get_classes(classes_file):
+    classes = []
+    with open(classes_file, 'r') as file:
+        for line in file:
+            classes.append(line.strip())
+
+    return classes
+
+def get_labels_from_file(labels_dir, lbl_name):
+    data = []
+    try:
+        with open(os.path.join(labels_dir, lbl_name), 'r') as file:
+            for line in file:
+                # Split the line by spaces and convert each part to a float
+                numbers = list(map(float, line.split()))
+                # Append the list of numbers to the data list
+                data.append(numbers)
+        return data
+    except:
+        return None
+
+def generate_ls_uid(length=10):
+    first_char = secrets.choice(string.ascii_lowercase)
+    other_chars = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(10))
+
+    return f'{first_char}_{other_chars}'
+
+def fill_in_annotations(data, classes, im_w, im_h):
+    results = []
+    for item in data:
+        class_name = classes[int(item[0])]
+
+        width = item[3]
+        height = item[4]
+        x = item[1] - (width / 2)
+        y = item[2] - (height / 2)
+
+        x *= 100
+        y *= 100
+        width *= 100
+        height *= 100
+
+        results.append({
+                    "id": f"{generate_ls_uid()}",
+                    "type": "rectanglelabels",
+
+                    "value": {
+                    "x": x,
+                    "y": y,
+                    "width": width,
+                    "height": height,
+                    "rotation": 0,
+                    "rectanglelabels": [class_name]
+                    },
+                    "origin": "manual",
+                    "to_name": "image",
+                    "from_name": "label",
+
+                    "image_rotation": 0,
+                    "original_width": im_w,
+                    "original_height": im_h
+                })
+
+    return results
+
+def add_annotations_from_tasks(proj_id):
+    classes = get_classes(classes_file)
+
+    tasks = get_tasks(proj_id)
+    for task in tqdm(tasks):
+        task_id = task.id
+        im_name = task.data['text']
+
+        root, _ = os.path.splitext(im_name)
+        lbl_name = f'{root}.txt'
+
+        img = np.array(Image.open(os.path.join(imgs_dir, im_name)))
+        im_h, im_w, _ = img.shape
+
+        data = get_labels_from_file(labels_dir, lbl_name)
+        if data:
+            results = fill_in_annotations(data, classes, im_w, im_h)
+        else:
+            results = []
+        ls.annotations.create(
+            id=task_id,
+            result= results,
+            was_cancelled=False,
+            ground_truth=True,
+        )
+
+# add_tasks_from_folder(proj_id, imgs_dir, 'package1')
+
+# add_annotations_from_tasks(proj_id)
