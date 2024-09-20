b045df34b589b517c1adf21cef6401cfbf2bd556
feras <fsalmasri@hotmail.com>
2024-09-20 11:35:06 +0200

    remove file1.txt

--- LS/__init__.py
+++
@@ -1,6 +0,0 @@
-from label_studio_sdk.client import LabelStudio
-
-
-LABEL_STUDIO_URL = 'https://safari.lisa.ulb.ovh'
-API_KEY = '19563f95209292aa545aa0845388c18ad476f30c'
-ls = LabelStudio(base_url=LABEL_STUDIO_URL, api_key=API_KEY)
